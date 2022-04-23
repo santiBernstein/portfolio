b827ab148695bab2917d5c59394e1149eb367439
santiBernstein <santiago.bernstein@gmail.com>
2022-04-23 17:27:36 -0300

    armado estructura general, menu mobile con estilos

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+// menu show //
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav--toggle','nav-menu');
+
+// active and remove menu //
+const navLink = document.querySelectorAll('.nav--link')
+
+function linkAction (){
+    //active link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    //remove menu mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
