f221980df3dab98eedef7ee681e0fa96eb4b8eb7
santiBernstein <santiago.bernstein@gmail.com>
2022-04-27 16:53:22 -0300

    armado scroll reveal en main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,44 @@ function linkAction (){
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// scroll reveal animation //
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+// sr home //
+sr.reveal('.home--title',{})
+sr.reveal('.button',{delay: 200})
+sr.reveal('.home--img',{delay: 400})
+sr.reveal('.home--social-icon',{interval: 200})
+
+//sr about //
+sr.reveal('.about--img',{})
+sr.reveal('.about--subtitle',{delay: 200})
+sr.reveal('.about--text',{delay: 400})
+
+//sr skills//
+sr.reveal('.skills--subtitle',{})
+sr.reveal('.skills--text',{delay: 200})
+sr.reveal('.skills--data',{interval: 200})
+sr.reveal('.skills--img',{delay: 400})
+
+//work//
+sr.reveal('.work--img',{interval: 200})
+
+//contact//
+sr.reveal('.contact--data',{interval: 200})
+
+
+
+
+
+
+
+
+
