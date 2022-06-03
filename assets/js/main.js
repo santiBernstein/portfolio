f6b535cd74a04defddbdd2ec24da77baa813ad23
santiBernstein <santiago.bernstein@gmail.com>
2022-06-03 19:38:18 -0300

    local storage theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,27 +78,38 @@ const toggleIcon = document.getElementById('toggle-icon');
 //light mode styles
 function lightMode (){
     toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.remove('bxs-moon');
-    toggleIcon.children[1].classList.add('bxs-sun');
+    toggleIcon.children[1].classList.replace('bxs-moon', 'bxs-sun');
 }
 
 //dark mode styles
 function darkMode (){
     toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.remove('bxs-sun');
-    toggleIcon.children[1].classList.add('bxs-moon');
+    toggleIcon.children[1].classList.replace('bxs-sun', 'bxs-moon');
 }
 
 //switch theme dynamically
 function switchTheme(event){
     if(event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();          
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
 
 // event listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// check local storage for theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
